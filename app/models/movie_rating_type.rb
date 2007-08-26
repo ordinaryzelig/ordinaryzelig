aed70466db649d3f9d74909783f74b18fd86d13d
ordinaryzelig <ordinaryzelig@487ece76-69b2-db11-9a89-001422107726>
2007-08-26 03:17:11 +0000

    order RatingType.container by option.value.
    git-svn-id: file:///Users/ningj/Documents/website/svn/trunk@389 487ece76-69b2-db11-9a89-001422107726

--- app/models/movie_rating_type.rb
+++ app/models/movie_rating_type.rb
@@ -1,11 +1,11 @@
 class MovieRatingType < ActiveRecord::Base
   
-  has_many :rating_options, :class_name => "MovieRatingOption", :foreign_key => "movie_rating_type_id"
+  has_many :rating_options, :class_name => "MovieRatingOption", :foreign_key => "movie_rating_type_id", :order => "value"
   validates_presence_of :name
   validates_uniqueness_of :name
   
   def container
-    rating_options.map { |option| ["#{option.value} - #{option.description}", option.value] }
+    rating_options.map { |option| ["#{option.value} - #{option.description}", option.value] }.sort { |a, b| a[1] <=> b[1] }
   end
   
 end
