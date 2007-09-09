1b513d17c5bb75eafe94b1e1ab03372305125f79
ordinaryzelig <ordinaryzelig@487ece76-69b2-db11-9a89-001422107726>
2007-09-09 08:54:03 +0000

    converted to active record sessions.
    git-svn-id: file:///Users/ningj/Documents/website/svn/trunk@465 487ece76-69b2-db11-9a89-001422107726

--- config/_environment.rb
+++ config/_environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.1.6'
+# RAILS_GEM_VERSION = '1.1.6'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -25,7 +25,7 @@ Rails::Initializer.run do |config|
 
   # Use the database for sessions instead of the file system
   # (create the session table with 'rake db:sessions:create')
-  # config.action_controller.session_store = :active_record_store
+  config.action_controller.session_store = :active_record_store
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper, 
