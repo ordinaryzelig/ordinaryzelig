7d6d4b0137e871f03db701ef4a22ced1061a6008
svn <svn@487ece76-69b2-db11-9a89-001422107726>
2007-09-20 03:24:47 +0000

    don't render anything if rss/user_recents doesn't find user.
    git-svn-id: file:///Users/ningj/Documents/website/svn/trunk@494 487ece76-69b2-db11-9a89-001422107726

--- app/controllers/rss_controller.rb
+++ app/controllers/rss_controller.rb
@@ -5,6 +5,7 @@ class RssController < ApplicationController
   
   def user_recents
     @user = User.find_by_secret_id(params[:id])
+    render(:nothing => true) and return unless @user
     @recents = remove_read_entities(@user.recents)
     headers['Content-Type'] = 'application/rss+xml'
   end
