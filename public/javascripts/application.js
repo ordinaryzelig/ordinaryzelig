a62089fb64927d1b6ffb9d27be206c045e2f0e22
svn <svn@487ece76-69b2-db11-9a89-001422107726>
2007-10-11 23:53:28 +0000

    moved switch_time_format code to application.js
    git-svn-id: file:///Users/ningj/Documents/website/svn/trunk@526 487ece76-69b2-db11-9a89-001422107726

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function switch_time_format(thingy)
+{
+    var temp = thingy.innerHTML;
+    thingy.innerHTML = thingy.title;
+    thingy.title = temp;
+    thingy.onclick = switch_time_format(thingy);
+}
