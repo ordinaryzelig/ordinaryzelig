e29d1503e210caf4460dc36a5524dbd657655917
svn <svn@487ece76-69b2-db11-9a89-001422107726>
2007-12-09 01:07:39 +0000

    blog can be created with privacy level.
    git-svn-id: file:///Users/ningj/Documents/website/svn/trunk@575 487ece76-69b2-db11-9a89-001422107726

--- vendor/plugins/has_privacy/lib/has_privacy.rb
+++ vendor/plugins/has_privacy/lib/has_privacy.rb
@@ -12,9 +12,9 @@ module OrdinaryZelig
         @has_privacy = true
         include InstanceMethods
         has_one :privacy_level, :as => :entity, :dependent => :destroy
-        before_save :add_privacy_level
-        after_update :save_privacy_level
-        validates_associated :privacy_level
+        # after_save :add_privacy_level
+        # after_update :save_privacy_level
+        # validates_associated :privacy_level
         attr_accessible :privacy_level_attributes
       end
       
@@ -29,15 +29,15 @@ module OrdinaryZelig
       # add privacy_level if not already added.
       # default privacy_level_type_id to 2 if not already set.
       def add_privacy_level(privacy_level_type_id = 2)
-        # self.privacy_level ||= build_privacy_level(:privacy_level_type_id => privacy_level_type_id)
+        self.privacy_level ||= build_privacy_level(:privacy_level_type_id => privacy_level_type_id)
       end
       
       def privacy_level_attributes=(attributes)
-        if self.privacy_level && self.privacy_level.new_record?
-          self.privacy_level.attributes = attributes
-        else
+        # if self.privacy_level && self.privacy_level.new_record?
+        #   self.privacy_level.attributes = attributes
+        # else
           build_privacy_level(attributes)
-        end
+        # end
       end
       
       def save_privacy_level
