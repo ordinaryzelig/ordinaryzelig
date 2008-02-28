1e7199099c97dfcfe3c99307200e7507e653ef06
svn <svn@487ece76-69b2-db11-9a89-001422107726>
2008-02-28 03:21:10 +0000

    pool_user_tests: test_points, test_bracket_completion.
    git-svn-id: file:///Users/ningj/Documents/website/svn/trunk@641 487ece76-69b2-db11-9a89-001422107726

--- test/unit/pool_user_test.rb
+++ test/unit/pool_user_test.rb
@@ -12,6 +12,30 @@ class PoolUserTest < Test::Unit::TestCase
     assert_equal PoolUser.master(Season.find(1)), master_pool_user
   end
   
+  def test_points
+    pool_user = pool_users(:cece_winner)
+    master_pics = pool_user.class.master(pool_user.season).pics
+    {ScoringSystems::Jared => 235,
+     ScoringSystems::Cbs => 63,
+     ScoringSystems::Espn => 630,
+     ScoringSystems::SportsIllustrated => 70}.each do |system, points|
+       assert_equal pool_user.calculate_points(master_pics, system), points
+    end
+  end
   
+  def test_bracket_complete
+    pool_user = pool_users(:cece_winner)
+    assert pool_user.bracket_complete?
+    pic = pool_user.pics.for_game(pool_user.season.root_game)
+    bid_id = pic.bid_id
+    pic.bid_id = nil
+    pic.save
+    assert_not pool_user.bracket_complete?
+    pic.bid_id = bid_id
+    pic.save
+    assert pool_user.bracket_complete?
+  end
+  
+  # here.
   
 end
